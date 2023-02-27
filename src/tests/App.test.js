dce2f579fbd2f8fc77f583baf7c0b3d3c778e420
João Ricardo Viana <joaorviana98@gmail.com>
2023-02-27 15:36:46 -0300

    Req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import render from './helper/render';
+import App from '../App';
+
+const strFav = 'Favorite Pokémon';
+describe('', () => {
+  it('Verifica os links', () => {
+    render(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0].text).toBe('Home');
+    expect(links[1].text).toBe('About');
+    expect(links[2].text).toBe(strFav);
+  });
+  it('Verifica se redireciona para Home', () => {
+    render(<App />);
+    const home = screen.getByText('Home');
+    userEvent.click(home);
+    const text = screen.getByText('Encountered Pokémon');
+    expect(text).toBeInTheDocument();
+  });
+  it('Verifica se redireciona para About', () => {
+    render(<App />);
+    const about = screen.getByText('About');
+    userEvent.click(about);
+    const text = screen.getByText('About Pokédex');
+    expect(text).toBeInTheDocument();
+  });
+  it('Verifica se rediciona para Favorite Pokémon', () => {
+    render(<App />);
+    const fav = screen.getByText(strFav);
+    userEvent.click(fav);
+    const text = screen.getByRole('heading', { name: strFav, level: 2 });
+    expect(text).toBeInTheDocument();
+  });
+});
---
+++ src/tests/helper/render.jsx
@@ -0,0 +1,14 @@
+import { render as defaultRender } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+function render(element, route = '/') {
+  const history = createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...defaultRender(<Router history={ history }>{element}</Router>),
+    history,
+  };
+}
+
+export default render;
