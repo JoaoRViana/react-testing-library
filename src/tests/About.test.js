d2d2d17440f628e7476d5d6a4be9b7c1dd28487b
João Ricardo Viana <joaorviana98@gmail.com>
2023-02-27 15:52:34 -0300

    req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import render from './helper/render';
+import { About } from '../pages';
+
+describe('', () => {
+  it('verifica se contém um heading', () => {
+    render(<About />);
+    const head = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(head).toBeInTheDocument();
+  });
+  it('verifica se a img está correta', () => {
+    render(<About />);
+    const img = screen.getByAltText('Pokédex');
+    console.log(img);
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
