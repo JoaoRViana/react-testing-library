c67a52c060257cea51340b64bb3c5361b18eb4bf
João Ricardo Viana <joaorviana98@gmail.com>
2023-02-27 16:16:57 -0300

    req3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -2,4 +2,24 @@ import { screen } from '@testing-library/react';
 import render from './helper/render';
 import { FavoritePokemon } from '../pages';
 
-describe('', () => {});
+describe('', () => {
+  const fakeChu = {
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: 6.0,
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
+  };
+  it('verifica mensagem sem pokémon favorito', () => {
+    render(<FavoritePokemon />);
+    const text = screen.getByText('No favorite Pokémon found');
+    expect(text).toBeInTheDocument(text);
+  });
+  it('verifica se retorna pokemon favoritado', () => {
+    render(<FavoritePokemon pokemonList={ [fakeChu] } />);
+    const text = screen.getByText('Pikachu');
+    expect(text).toBeInTheDocument();
+  });
+});
